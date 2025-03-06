215ce7ca96aee60c7f86507250a80da19485f52c
Sebastián Sanhueza Tapia <sebastian.sanhueza.t@usach.cl>
2025-03-06 16:07:18 -0300

    Uso de un Hook, el useState, para manejar el estado de un input.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Card from './components/Card'
 import './App.css'
+import Counter from './components/Counter'
 
 function App() {
   const items = ["React", "Vite", "TypeScript"];
@@ -9,6 +11,10 @@ function App() {
   return (
     <>
       <h1>Hola Mundo React</h1>
+      <Card title="Tarjeta 1" description="Esta es la descripción de la tarjeta 1" />
+      <Card title="Tarjeta 2" description="Esta es la descripción de la tarjeta 2" />
+      <Card title="Tarjeta 3" description="Esta es la descripción de la tarjeta 3" />
+      <Counter />
       {
         items.map((item, index) => (
           <li key={index}>{item}</li>
