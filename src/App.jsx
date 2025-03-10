e511931769d23f237a9d7ea97a1f34392a49b967
Sebastián Sanhueza Tapia <sebastian.sanhueza.t@usach.cl>
2025-03-10 13:00:03 -0300

    uso de css en una card y de module.css para dar estados locales a los elementos

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Card from './components/Card'
+import Card from './components/Card/Card'
 import './App.css'
 import Counter from './components/Counter'
+import ToggleButton from './components/ToggleButton'
+import NameForm from './components/NameForm'
+import CounterWithEffect from './components/CounterWithEffect'
 
 function App() {
   const items = ["React", "Vite", "TypeScript"];
@@ -11,6 +14,9 @@ function App() {
   return (
     <>
       <h1>Hola Mundo React</h1>
+      <CounterWithEffect />
+      <NameForm />
+      <ToggleButton />
       <Card title="Tarjeta 1" description="Esta es la descripción de la tarjeta 1" />
       <Card title="Tarjeta 2" description="Esta es la descripción de la tarjeta 2" />
       <Card title="Tarjeta 3" description="Esta es la descripción de la tarjeta 3" />
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,6 +1,9 @@
+// import '../styles.css'
+import { card } from './Card.module.css'
+
 const Card = ({ title, description }) => {
     return (
-        <section>
+        <section className={card}>
             <h2>{title}</h2>
             <p>{description}</p>
         </section>
