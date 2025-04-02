f981d637fff80fe0b7418b78e6b7c6758165540e
Sebastián Sanhueza Tapia <sebastian.sanhueza.t@usach.cl>
2025-04-02 17:04:30 -0300

    button en ts

--- src/components/ButtonTs.tsx
+++ src/components/ButtonTs.tsx
@@ -7,7 +7,9 @@ const ButtonTs = ({ onclick, label }: ButtonProps) => {
     return (
         <button 
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            onClick={onclick}>{label}</button>);
+            onClick={onclick}>{label}
+        </button>
+    );
 };
 
 export default ButtonTs;
